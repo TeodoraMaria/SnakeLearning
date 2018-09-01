47dbfa1ff2784bb16cee14f281d1bae4768afeb3
Emil Terman <terman.emil@gmail.com>
2018-09-01 22:54:18 +0300

    Added an IRenderer in GameLogic. It must be moved in GameView

---
+++ includes/GameLogic/TermRenderer.hpp
@@ -0,0 +1,12 @@
+//
+//  TermRenderer.hpp
+//  Project
+//
+//  Created by Emil Terman on 9/1/18.
+//
+
+#ifndef TermRenderer_h
+#define TermRenderer_h
+
+
+#endif /* TermRenderer_h */
---
+++ src/GameLogic/TermRenderer.cpp
@@ -0,0 +1,8 @@
+//
+//  TermRenderer.cpp
+//  GameLogic
+//
+//  Created by Emil Terman on 9/1/18.
+//
+
+#include <stdio.h>
