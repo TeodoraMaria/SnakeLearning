dda2abf799b780b6f14df03aa0dd5d46023bbc4c
Emil Terman <terman.emil@gmail.com>
2018-09-03 14:15:03 +0300

    The Multiplatform::getch() had some weird duplicate code.

--- src/GameLogic/MultiPlatformColoredText.cpp
+++ src/GameLogic/MultiPlatformColoredText.cpp
@@ -10,7 +10,7 @@
 #endif
 
 #ifndef _WIN32
-	int getch()
+	int UnixGetch()
 	{
 		int buf = 0;
 		struct termios old = { 0, 0};
@@ -96,34 +96,10 @@ void MultiPlatform::PrintColoredStr(const Color color, const std::string str)
 int MultiPlatform::getch()
 {
 	#ifdef _WIN32
-		
 		const auto result = ::getch();
 		::getch();
 		return result;
 	#else
-		int buf = 0;
-		struct termios old = { 0 };
-	
-		if(tcgetattr(0, &old) < 0)
-			perror("tcsetattr()");
-	
-		old.c_lflag &= ~ICANON;
-		old.c_lflag &= ~ECHO;
-		old.c_cc[VMIN] = 1;
-		old.c_cc[VTIME] = 0;
-	
-		if(tcsetattr(0, TCSANOW, &old) < 0)
-			perror("tcsetattr ICANON");
-	
-		if(read(0, &buf, 1) < 0)
-			perror("read()");
-	
-		old.c_lflag |= ICANON;
-		old.c_lflag |= ECHO;
-	
-		if(tcsetattr(0, TCSADRAIN, &old) < 0)
-			perror("tcsetattr ~ICANON");
-	
-		return buf;
+		return UnixGetch();
 	#endif
 }
