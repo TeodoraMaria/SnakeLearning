38038e9350380b22f15a31bb776f6cc29a1048c0
Cristi <thiess.cristi@gmail.com>
2018-09-03 17:27:54 +0300

    implemented the opengl renderer for now, needs to be integrated

--- includes/GameView/OpenGLRenderer.h
+++ includes/GameView/OpenGLRenderer.h
@@ -1,11 +1,22 @@
 #include "TermRenderer.hpp"
+#include "Board.h"
 
 namespace GameView
 {
-   class OpenGLRendered :public IGameRenderer
+   class OpenGLRenderer :public IGameRenderer
    {
+   public:
+
+      OpenGLRenderer(Board* board);
+
       // Inherited via IGameRenderer
       virtual void Render(const GameState & gameState) const override;
+
+   private:
+      void updateBoard(const GameState& gameState) const;
+
+      Board* m_board;
+
    };
 
 
