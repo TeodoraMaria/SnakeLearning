fec1a07d10855ac660ca89ac292445ac80117d64
Cristi <thiess.cristi@gmail.com>
2018-09-04 12:38:01 +0300

    added some base classes for the genetic algorithm bot

---
+++ src/AI/GeneticAlgorithm/GeneticBot.cpp
@@ -0,0 +1,8 @@
+#include "GeneticBot.h"
+
+using namespace AI::GeneticAlgorithm;
+
+SnakeMove GeneticBot::GetNextAction(const GameState & gameState) const
+{
+    return SnakeMove();
+}
---
+++ src/AI/GeneticAlgorithm/GeneticTrainer.cpp
@@ -0,0 +1,8 @@
+#include "AI/GeneticAlgorithm/GeneticTrainer.h"
+
+using namespace AI::GeneticAlgorithm;
+
+IPlayer *GeneticTrainer::Train()
+{
+    return nullptr;
+}
