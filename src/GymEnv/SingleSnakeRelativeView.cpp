6ba39c5894a0c60c3d5b65c31045b60ba1a71994
Emil Terman <terman.emil@gmail.com>
2018-09-06 01:44:39 +0300

    Fixed a minor issue

--- src/GymEnv/SingleSnakeRelativeView.cpp
+++ src/GymEnv/SingleSnakeRelativeView.cpp
@@ -32,10 +32,8 @@ std::vector<double> SingleSnakeRelativeView::GetState() const
 	const auto gmState = m_game->GetGameState();
 	const auto snake = gmState.GetSnake(m_student->GetSnakeNumber());
 	
-	const auto containerLen =
-		3 * (m_celInterpreter->NbOfInterpretableCells() - 1);
-	
-	auto observationContainer = std::vector<double>(containerLen);
+	auto observationContainer =
+		std::vector<double>(m_stateObserver->NbOfObservations());
 	
 	m_stateObserver->Observe(
 		observationContainer,
