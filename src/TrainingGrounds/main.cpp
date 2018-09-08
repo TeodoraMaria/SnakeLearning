95052c7581946b66ddda41a497333d92294d1d5c
Emil Terman <terman.emil@gmail.com>
2018-09-08 19:20:23 +0300

    Failed to compile Genetics

--- src/TrainingGrounds/main.cpp
+++ src/TrainingGrounds/main.cpp
@@ -3,7 +3,7 @@
 #include "AI/HardCoded/SingleBot.hpp"
 #include "AI/GeneticAlgorithm/GeneticTrainer.h"
 #include "AI/GeneticAlgorithm/GeneticOptions.h"
-
+#include "AI/QLearning/MultisnakeTabularTrainer.hpp"
 
 #include "GymEnv/SingleSnakeRelativeView.hpp"
 #include "GymEnv/SingleSnakeGridView.hpp"
@@ -201,35 +201,92 @@ void MainSingleSnakeGridView()
 //		game.Play();
 }
 
-void GeneticSingleSnake()
+// void GeneticSingleSnake()
+// {
+//     auto gmOptions = GameOptions();
+//     {
+//         gmOptions.boardLength = 25;
+//         gmOptions.boardWidth = 25;
+//         gmOptions.numFoods = 10;
+//     }
+//     auto gmRenderer = new GameView::TermRenderer();
+
+//     auto baseModel = GymEnv::SingleSnakeEnvBaseModel();
+
+//     baseModel.gmOptions = &gmOptions;
+//     baseModel.gmRenderer = gmRenderer;
+//     baseModel.celInterpreter = std::make_shared<CellInterpreter::WallFoodBody>();
+
+//     auto env = new GymEnv::SingleSnakeRelativeView(baseModel);
+
+//     AI::GeneticAlgorithm::GeneticOptions options;
+
+//     options.crossoverProb = 0.4;
+//     options.maxNumSteps = 100;
+//     options.mutationProb = 0.04;
+//     options.numEpisodes = 1000;
+//     options.numOfNetworks = 50;
+
+//     auto trainer = AI::GeneticAlgorithm::GeneticTrainer(options, env);
+//     trainer.Train();
+
+// }
+
+void MultisnakeMain()
 {
-    auto gmOptions = GameOptions();
-    {
-        gmOptions.boardLength = 25;
-        gmOptions.boardWidth = 25;
-        gmOptions.numFoods = 10;
-    }
-    auto gmRenderer = new GameView::TermRenderer();
-
-    auto baseModel = GymEnv::SingleSnakeEnvBaseModel();
-
-    baseModel.gmOptions = &gmOptions;
-    baseModel.gmRenderer = gmRenderer;
-    baseModel.celInterpreter = std::make_shared<CellInterpreter::WallFoodBody>();
-
-    auto env = new GymEnv::SingleSnakeRelativeView(baseModel);
-
-    AI::GeneticAlgorithm::GeneticOptions options;
-
-    options.crossoverProb = 0.4;
-    options.maxNumSteps = 100;
-    options.mutationProb = 0.04;
-    options.numEpisodes = 1000;
-    options.numOfNetworks = 50;
-
-    auto trainer = AI::GeneticAlgorithm::GeneticTrainer(options, env);
-    trainer.Train();
-
+	auto gmOptions = GameOptions();
+	{
+		gmOptions.boardLength = 25;
+		gmOptions.boardWidth = 25;
+		gmOptions.numFoods = 10;
+		gmOptions.initialSnakeSize = 3;
+//		gmOptions.gameRenderer = new GameView::TermRenderer();
+		gmOptions.gameRenderer = new GameView::OpenGLRenderer(
+			500, 500,
+			gmOptions.boardLength, gmOptions.boardWidth);
+	}
+	
+	auto baseModel = GymEnv::SingleSnakeEnvBaseModel();
+	{
+		baseModel.gmRenderer = gmOptions.gameRenderer;
+		baseModel.gmOptions = &gmOptions;
+		baseModel.celInterpreter = std::make_shared<CellInterpreter::Basic3CellInterpreter>();
+//		baseModel.celInterpreter = std::make_shared<CellInterpreter::WallFoodBody>();
+	}
+	
+	auto qoptions = AI::QLearning::QOptions();
+	{
+		qoptions.maxNumSteps = [&](int episode)
+		{
+			return 50 + (double)episode / qoptions.numEpisodes * 1000;
+		};
+		qoptions.qDiscountFactor = 0.8;
+		qoptions.actionQualityEps = 0.005;
+		
+		qoptions.numEpisodes = 20000;
+		qoptions.randActionDecayFactor = 1.0 / (qoptions.numEpisodes);
+		qoptions.learningRate = 0.01;
+		qoptions.minRandActionChance = 0;
+		qoptions.maxStepsWithoutFood = [&](int episode) -> size_t
+		{
+			return 50u + (double)episode / qoptions.numEpisodes * 100.0;
+		};
+		
+		qoptions.foodReward = [](int episode) { return 1.0; };
+		qoptions.dieReward = [](int episode) { return 0; };
+		qoptions.stepReward = [](int episode) { return -0.0001; };
+		
+//		auto qInitDistrib = std::uniform_real_distribution<>(-1.0, 1.0);
+		qoptions.tabInitializer = [&](std::mt19937& merseneTwister)
+		{
+			return 0;
+//			return qInitDistrib(merseneTwister);
+		};
+		qoptions.milsToSleepBetweenFrames = 10;
+	}
+	
+	auto trainer = AI::QLearning::MultisnakeTabularTrainer(gmOptions, qoptions);
+	trainer.Train();
 }
 
 int main(int nargs, char** args)
@@ -237,7 +294,8 @@ int main(int nargs, char** args)
 	srand(time(nullptr));
 	
 //	MainSingleSnakeRelativeView();
-	MainSingleSnakeGridView();
+//	MainSingleSnakeGridView();
+	MultisnakeMain();
 
 //    GeneticSingleSnake();
 	
