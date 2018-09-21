6fc9ae065ee4efacd2215239f9837bcfd8dac1bf
Emil Terman <terman.emil@gmail.com>
2018-09-21 09:16:14 +0300

    It started to lean somewhat

--- src/TensorflowSandbox/main.cpp
+++ src/TensorflowSandbox/main.cpp
@@ -57,7 +57,7 @@ using namespace GymEnv::Utils;
 
 int main()
 {
-	const auto learningRate = 0.1;
+	const auto learningRate = 0.01;
 
 	auto cellInterpreter = std::make_shared<CellInterpreter::Basic3CellInterpreter>();
 	auto observer = std::make_shared<GridObserver>(cellInterpreter, 3, 3);
@@ -76,64 +76,83 @@ int main()
 	const auto layersSizes = std::array<int, 2>{{ numInputs * 2, (numInputs + numOutputs) * 2 }};
 	
 	// Weights.
-	const int shape1[] = { numInputs, layersSizes[0] };
+	const int shape1[] = { numInputs, numOutputs };
 	auto w1 = Variable(scope, { shape1[0], shape1[1] }, DataType::DT_FLOAT);
 	auto assignW1 = Assign(scope, w1, RandomNormal(scope, { shape1[0], shape1[1] }, DataType::DT_FLOAT));
 	
-	const int shape2[] = { layersSizes[0], layersSizes[1] };
-	auto w2 = Variable(scope, { shape2[0], shape2[1] }, DataType::DT_FLOAT);
-	auto assignW2 = Assign(scope, w2, RandomNormal(scope, { shape2[0], shape2[1] }, DataType::DT_FLOAT));
-
-	const int shape3[] = { layersSizes[1], numOutputs };
-	auto w3 = Variable(scope, { shape3[0], shape3[1] }, DataType::DT_FLOAT);
-	auto assignW3 = Assign(scope, w3, RandomNormal(scope, { shape3[0], shape3[1] }, DataType::DT_FLOAT));
+//	const int shape2[] = { layersSizes[0], layersSizes[1] };
+//	auto w2 = Variable(scope, { shape2[0], shape2[1] }, DataType::DT_FLOAT);
+//	auto assignW2 = Assign(scope, w2, RandomNormal(scope, { shape2[0], shape2[1] }, DataType::DT_FLOAT));
+//
+//	const int shape3[] = { layersSizes[1], numOutputs };
+//	auto w3 = Variable(scope, { shape3[0], shape3[1] }, DataType::DT_FLOAT);
+//	auto assignW3 = Assign(scope, w3, RandomNormal(scope, { shape3[0], shape3[1] }, DataType::DT_FLOAT));
 	
 	// Bias.
 	auto b1 = Variable(scope, { 1, shape1[1] }, DataType::DT_FLOAT);
 	auto assignB1 = Assign(scope, b1, RandomNormal(scope, { 1, shape1[1] }, DataType::DT_FLOAT));
 	
-	auto b2 = Variable(scope, { 1, shape2[1] }, DataType::DT_FLOAT);
-	auto assignB2 = Assign(scope, b2, RandomNormal(scope, { 1, shape2[1] }, DataType::DT_FLOAT));
-
-	auto b3 = Variable(scope, { 1, shape3[1] }, DataType::DT_FLOAT);
-	auto assignB3 = Assign(scope, b3, RandomNormal(scope, { 1, shape3[1] }, DataType::DT_FLOAT));
+//	auto b2 = Variable(scope, { 1, shape2[1] }, DataType::DT_FLOAT);
+//	auto assignB2 = Assign(scope, b2, RandomNormal(scope, { 1, shape2[1] }, DataType::DT_FLOAT));
+//
+//	auto b3 = Variable(scope, { 1, shape3[1] }, DataType::DT_FLOAT);
+//	auto assignB3 = Assign(scope, b3, RandomNormal(scope, { 1, shape3[1] }, DataType::DT_FLOAT));
 	
 	// Layers.
-	auto layer1 = Tanh(scope, Add(scope, MatMul(scope, input, w1), b1));
-	auto layer2 = Tanh(scope, Add(scope, MatMul(scope, layer1, w2), b2));
-	auto layer3 = Tanh(scope, Add(scope, MatMul(scope, layer2, w3), b3));
+	auto layer1 = Add(scope, MatMul(scope, input, w1), b1);
+//	auto layer2 = Tanh(scope, Add(scope, MatMul(scope, layer1, w2), b2));
+//	auto layer3 = Tanh(scope, Add(scope, MatMul(scope, layer2, w3), b3));
+//	auto layer3 = Add(scope, MatMul(scope, layer2, w3), b3);
+	
+	auto& lastLayer = layer1;
+	
+	auto predict = ArgMax(scope, lastLayer, 1);
+	auto nextQ = Placeholder(scope, DataType::DT_FLOAT);
+	auto nextQTensor = Tensor(DataTypeToEnum<float>::v(), TensorShape{ 1, (int)IPlayer::possibleMoves.size() });
 	
 	auto reqularization = AddN(
 		scope,
-		std::initializer_list<Input>{ L2Loss(scope, w1), L2Loss(scope, w2), L2Loss(scope, w3)}
+		std::initializer_list<Input>{ L2Loss(scope, w1) }// L2Loss(scope, w2), L2Loss(scope, w3)}
 	);
-	auto loss = Add(
-		scope,
-		ReduceMean(scope, Square(scope, Sub(scope, layer3, output)), { 0, 1 }),
-		Mul(scope, Cast(scope, learningRate, DataType::DT_FLOAT), reqularization)
-	);
+//	auto loss = Add(
+//		scope,
+//		ReduceMean(scope, Square(scope, Sub(scope, layer3, output)), { 0, 1 }),
+//		Mul(scope, Cast(scope, learningRate, DataType::DT_FLOAT), reqularization)
+//	);
+	
+	auto loss = ReduceMean(scope, Square(scope, Sub(scope, lastLayer, nextQ)), { 0, 1 });
 	
 	/*
 	** Backpropagation.
 	*/
 	
 	std::vector<Output> gradOutputs;
-	TF_CHECK_OK(AddSymbolicGradients(scope, { loss }, { w1, w2, w3, b1, b2, b3 }, &gradOutputs));
+	TF_CHECK_OK(AddSymbolicGradients(
+		scope,
+		{ loss },
+		{
+			w1, // w2, w3,
+			b1, // b2, b3
+		},
+		&gradOutputs));
 	
 	auto applyW1 = ApplyGradientDescent(scope, w1, Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[0] });
-	auto applyW2 = ApplyGradientDescent(scope, w2, Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[1] });
-	auto applyW3 = ApplyGradientDescent(scope, w3, Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[2] });
+//	auto applyW2 = ApplyGradientDescent(scope, w2, Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[1] });
+//	auto applyW3 = ApplyGradientDescent(scope, w3, Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[2] });
 
-	auto applyB1 = ApplyGradientDescent(scope, b1, Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[3] });
-	auto applyB2 = ApplyGradientDescent(scope, b2, Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[4] });
-	auto applyB3 = ApplyGradientDescent(scope, b3, Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[5] });
+	auto applyB1 = ApplyGradientDescent(scope, b1, Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[1] });
+//	auto applyB2 = ApplyGradientDescent(scope, b2, Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[4] });
+//	auto applyB3 = ApplyGradientDescent(scope, b3, Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[5] });
 
 	ClientSession session(scope);
 	std::vector<Tensor> tfOutputs;
 
 	// Init the weights and biases by running the assigns nodes once.
 	TF_CHECK_OK(session.Run(
-		{ assignW1, assignW2, assignW3, assignB1, assignB2, assignB3 },
+		{
+			assignW1, // assignW2, assignW3,
+			assignB1, // assignB2, assignB3
+		},
 		nullptr
 	));
 
@@ -162,8 +181,8 @@ int main()
 		qoptions.qDiscountFactor = 0.9;
 		qoptions.actionQualityEps = 0.005;
 
-		qoptions.numEpisodes = 10000;
-		qoptions.randActionDecayFactor = 1.0 / (qoptions.numEpisodes / 4);
+		qoptions.numEpisodes = 1000;
+		qoptions.randActionDecayFactor = 1.0 / (50);
 		qoptions.learningRate = 0.1;
 		qoptions.maxRandActionChance = 1.0;
 		qoptions.minRandActionChance = 0.001;
@@ -230,14 +249,11 @@ int main()
 				
 				observer->Observe(observationContainer, gmState, snakeId);
 				std::copy_n(observationContainer.begin(), observer->NbOfObservations(), inputTensor.flat<float>().data());
-				TF_CHECK_OK(session.Run({{ input, inputTensor }}, { layer3 }, &tfOutputs));
+				TF_CHECK_OK(session.Run({{ input, inputTensor }}, { lastLayer }, &tfOutputs));
 				
-//				LOG(INFO) << tfOutputs[0].DebugString() << std::endl;
-//				std::vector<Tensor> tfOutputsPrint;
-//				TF_CHECK_OK(session.Run({}, { w1 }, &tfOutputsPrint));
-//				LOG(INFO) << w1.node()->DebugString() << std::endl;
-//				LOG(INFO) << w2 << std::endl;
-//				LOG(INFO) << w3 << std::endl;
+				std::vector<Tensor> tfOutputsPrint;
+				TF_CHECK_OK(session.Run({}, { w1 }, &tfOutputsPrint));
+				std::cout << tfOutputsPrint[0].DebugString() << std::endl;
 				
 				if (episode >= qoptions.numEpisodes - qoptions.lastNGamesToRender)
 				{
@@ -294,33 +310,51 @@ int main()
 					const auto gmState = game.GetGameState();
 					observer->Observe(nextObservationContainer, gmState, snakeId);
 					std::copy_n(nextObservationContainer.begin(), observer->NbOfObservations(), inputTensor.flat<float>().data());
-					TF_CHECK_OK(session.Run({{ input, inputTensor }}, { layer3 }, &tfOutputs));
+					TF_CHECK_OK(session.Run({{ input, inputTensor }}, { lastLayer, predict }, &tfOutputs));
 					
 					const auto networkOutput = tfOutputs[0].flat<float>().data();
 					const auto bestMoveQ = std::max_element(networkOutput, networkOutput + IPlayer::possibleMoves.size());
+					std::cout
+						<< tfOutputs[1].DebugString() << " || "
+						<< tfOutputs[0].DebugString() << " || "
+						<< std::distance(networkOutput, bestMoveQ) << std::endl;
 					bestNextQ = *bestMoveQ;
 				}
 				
 				std::copy_n(observationContainer.begin(), observer->NbOfObservations(), inputTensor.flat<float>().data());
 				
-				std::copy_n(networkOutput, IPlayer::possibleMoves.size(), requiredOutput.begin());
+//				std::copy_n(networkOutput, IPlayer::possibleMoves.size(), requiredOutput.begin());
+				std::fill(requiredOutput.begin(), requiredOutput.end(), 0);
 				requiredOutput[bestMoveIndx] = reward + qoptions.qDiscountFactor * bestNextQ;
 				
+				std::copy_n(requiredOutput.begin(), IPlayer::possibleMoves.size(), nextQTensor.flat<float>().data());
 				std::copy_n(requiredOutput.begin(), IPlayer::possibleMoves.size(), outputTensor.flat<float>().data());
 				
+//				TF_CHECK_OK(session.Run(
+//					{{ input, inputTensor }, { output, outputTensor }},
+//					{
+//						applyW1, applyW2, applyW3,
+//						applyB1, applyB2, applyB3,
+//						layer3
+//					},
+//					nullptr
+//				));
+				
 				TF_CHECK_OK(session.Run(
-					{{ input, inputTensor }, { output, outputTensor }},
+					{{ input, inputTensor }, { nextQ, nextQTensor }, { output, outputTensor }},
 					{
-						applyW1, applyW2, applyW3,
-						applyB1, applyB2, applyB3,
-						layer3
+						applyW1, // applyW2, applyW3,
+						applyB1, // applyB2, applyB3,
+						lastLayer, loss
 					},
-					nullptr
+					&tfOutputsPrint
 				));
+				
+				std::cout << "[LOSS]: " << tfOutputsPrint[3].DebugString() << std::endl;
 			}
-			
-			noise = ::Utils::Math::Lerp(noise, qoptions.minRandActionChance, qoptions.randActionDecayFactor);
 		}
+		
+		noise = ::Utils::Math::Lerp(noise, qoptions.minRandActionChance, qoptions.randActionDecayFactor);
 
 		printf(
 			"End of episode: %d with a reward of %.2f. Noise: %.2f\n",
