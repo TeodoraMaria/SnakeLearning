19eb380b2417fdcefecc6c5c1e680217d372c43e
Cristi <thiess.cristi@gmail.com>
2018-09-23 10:22:13 +0300

    added from_json and to_json for ThreeDirectionalObserver

--- includes/ConfigLoading/IObserverJson.h
+++ includes/ConfigLoading/IObserverJson.h
@@ -2,6 +2,7 @@
 
 #include "GymEnv/StateObserver/IStateObserver.hpp"
 #include "GymEnv/StateObserver/GridObserver.hpp"
+#include "GymEnv/StateObserver/ThreeDirectionalObserver.hpp"
 #include <json.hpp>
 #include <memory>
 
@@ -9,7 +10,9 @@ namespace GymEnv { namespace StateObserver
 {
 	void to_json(nlohmann::json& j, const IStateObserver* observer);
 	void to_json(nlohmann::json& j, const GridObserver* observer);
+    void to_json(nlohmann::json& j, const ThreeDirectionalObserver* observer);
 
 	void from_json(const nlohmann::json& j, std::shared_ptr<IStateObserver>& observer);
 	void from_json(const nlohmann::json& j, std::shared_ptr<GridObserver>& observer);
+    void from_json(const nlohmann::json& j, std::shared_ptr<ThreeDirectionalObserver>& observer);
 }}
