14a77dacf118cb23f128657db44eea4049c70666
Cristi <thiess.cristi@gmail.com>
2018-09-26 19:19:15 +0300

    split qlearning into deep and tabular for graphic representation

--- src/AppUI/OptionsScene.cpp
+++ src/AppUI/OptionsScene.cpp
@@ -25,10 +25,11 @@ void OptionsScene::createScene()
 
     m_ui->spinBoxHumans->setValue(m_gameSettings.nbHumanPlayers);
     m_ui->spinBoxGeneticBots->setValue(m_gameSettings.nbGeneticBots);
-    m_ui->spinBoxQlearningBots->setValue(m_gameSettings.nbQlearningBots);
+    m_ui->spinBoxQlearningDeepBots->setValue(m_gameSettings.nbQlearningDeepBots);
+    m_ui->spinBoxQlearningTabularBots->setValue(m_gameSettings.nbQlearningTabularBots);
     m_ui->spinBoxSupervisedBots->setValue(m_gameSettings.nbSupervisedBots);
     m_ui->spinBoxNormalBots->setValue(m_gameSettings.nbNormalBots);
-
+    
     m_ui->spinBoxMapWidth->setValue(m_gameSettings.mapWidth);
     m_ui->spinBoxMapHeight->setValue(m_gameSettings.mapHeight);
     m_ui->spinBoxFoodCount->setValue(m_gameSettings.foodCount);
@@ -46,7 +47,7 @@ void OptionsScene::quitButtonPressed()
 {   
     m_gameSettings.nbHumanPlayers = m_ui->spinBoxHumans->value();
     m_gameSettings.nbGeneticBots = m_ui->spinBoxGeneticBots->value();
-    m_gameSettings.nbQlearningBots = m_ui->spinBoxQlearningBots->value();
+    m_gameSettings.nbQlearningDeepBots = m_ui->spinBoxQlearningBots->value();
     m_gameSettings.nbSupervisedBots = m_ui->spinBoxSupervisedBots->value();
     m_gameSettings.nbNormalBots = m_ui->spinBoxNormalBots->value();
 
--- src/AppUI/TrainScene.cpp
+++ src/AppUI/TrainScene.cpp
@@ -54,10 +54,6 @@ void TrainScene::createScene()
 
     m_centralWidget = ui->centralwidget;
     
-    m_maxFitnessValues = new QLineSeries();
-   // m_maxFitnessValues->setName("Max Fitness");
-    m_avgFitnessValues = new QLineSeries();
-    //m_avgFitnessValues->setName("Avg. Fitness");
 
     m_graphValues.resize(200);
 
@@ -76,8 +72,6 @@ void TrainScene::createScene()
     }
 
     m_chart->legend()->hide();
-    m_chart->addSeries(m_maxFitnessValues);
-    m_chart->addSeries(m_avgFitnessValues);
     m_chart->createDefaultAxes();
 
     m_chart->setTitle("Snake Generations");
@@ -113,6 +107,7 @@ void TrainScene::backButtonPressed()
 
 void TrainScene::startButtonPressed()
 {
+    
     auto selectedAlg=ui->comboBoxAlgorithm->currentText();  
     auto s = selectedAlg.toStdString();
     std::string filePath = "./aux_files/";
