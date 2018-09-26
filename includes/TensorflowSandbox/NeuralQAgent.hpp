b2e5f0b093e7449315224825e2d3c5199893c162
Emil Terman <terman.emil@gmail.com>
2018-09-26 10:48:54 +0300

    Separated the NeuralQAgent in a separate file.

---
+++ includes/TensorflowSandbox/NeuralQAgent.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "GameLogic/IPlayer.h"
+#include "GymEnv/StateObserver/IStateObserver.hpp"
+
+#include <tensorflow/cc/client/client_session.h>
+#include <tensorflow/cc/framework/gradients.h>
+#include <tensorflow/core/framework/tensor.h>
+#include <tensorflow/cc/ops/array_ops.h>
+#include <json_fwd.hpp>
+
+#include <random>
+#include <memory>
+
+namespace AI { namespace QLearning
+{
+	using namespace tensorflow;
+	
+	class NeuralQAgent : public IPlayer
+	{
+	public:
+		NeuralQAgent(
+			std::shared_ptr<Scope> scope_in,
+			std::shared_ptr<ClientSession> session_in,
+			std::shared_ptr<GymEnv::StateObserver::IStateObserver> observer_in
+		);
+		
+		int NumInputs() const;
+		int NumOutputs() const;
+
+		SnakeMove GetNextAction(const GameState& gameState) override;
+
+		friend void to_json(nlohmann::json& j, NeuralQAgent* agent);
+		friend void from_json(const nlohmann::json& j, std::shared_ptr<NeuralQAgent> agent);
+
+		void LoadWeights(std::string filePath);
+		void SaveWeights(std::string filePath, bool compact = false);
+
+		int GetNextActionIndex(
+			const GameState& gmState,
+			double noise,
+			std::mt19937& merseneTwister,
+			double actionEqualityTreshold);
+
+		Tensor PredictBatch(Tensor inputBatch);
+
+		void Train(const Tensor& inputBatch, const Tensor& outputBatch);
+
+		std::shared_ptr<GymEnv::StateObserver::IStateObserver> observer;
+		std::vector<double> observationContainer;
+		std::vector<double> nextObservationContainer;
+
+		// Things used only for performance (after training).
+		double internalNoise = 0.01;
+		double internalActionEqualityTreshold = 0.01;
+		std::mt19937 internalMerseneTwister = std::mt19937(std::random_device()());
+
+	private:
+		Tensor observationTensor;
+
+		std::shared_ptr<Scope> scope;
+		std::shared_ptr<ClientSession> session;
+
+		ops::Placeholder inputHolder;
+		ops::Placeholder targetQHolder;
+		Output networkOutput;
+		Output loss;
+
+		std::vector<Output> variables;
+		std::vector<Output> appliedGrads;
+	};
+	
+}}
--- src/TensorflowSandbox/main.cpp
+++ src/TensorflowSandbox/main.cpp
@@ -1,3 +1,5 @@
+#include "NeuralQAgent.hpp"
+
 #include "GameLogic/CellInterpreter/Basic3CellInterpreter.hpp"
 #include "GymEnv/StateObserver/GridObserver.hpp"
 #include "GameLogic/IPlayer.h"
@@ -39,6 +41,11 @@
 #include "Utils/PrintUtils.h"
 #include "Utils/MatrixUtils.h"
 
+#include "ConfigLoading/QTabStudentJson.h"
+#include "ConfigLoading/IObserverJson.h"
+#include "ConfigLoading/ICellInterpreterJson.h"
+#include "AI/QLearning/TabularQJsonUtils.h"
+
 #include <chrono>
 #include <thread>
 #include <algorithm>
@@ -48,12 +55,10 @@
 #include <fstream>
 #include <iomanip>
 #include <numeric>
+#include <unordered_map>
 #include <json.hpp>
 
 using json = nlohmann::json;
-
-//auto g_debugging = true;
-auto g_debugging = false;
 
 using namespace tensorflow;
 using namespace tensorflow::ops;
@@ -65,6 +70,9 @@ using namespace GymEnv::StateObserver;
 using namespace GymEnv::Utils;
 
 static const int NoneFlag = -42;
+//static auto g_debugging = true;
+static auto g_debugging = false;
+
 
 GameOptions GetGameOptions()
 {
@@ -91,11 +99,11 @@ QOptions GetQOptions()
 	qoptions.qDiscountFactor = 0.95;
 	qoptions.actionQualityEps = 0.05;
 
-	qoptions.numEpisodes = 500;
+	qoptions.numEpisodes = 10000;
 	qoptions.learningRate = 0.0001;
 	
 	// Percentage of mean.
-	qoptions.maxNoise = 10.0;
+	qoptions.maxNoise = 1.5;
 	qoptions.minNoise = 0.02;
 	qoptions.noiseDecayFactor = 1.0 / (qoptions.numEpisodes * 0.1);
 	
@@ -116,23 +124,6 @@ QOptions GetQOptions()
 	qoptions.lastNGamesToRender = 2;
 
 	return qoptions;
-}
-
-int ProbabilisticallyPickAction(
-	const std::vector<float>& probabilities,
-	std::mt19937& merseneTwister)
-{
-	auto chanceDistrib = std::uniform_real_distribution<float>(0, 1.0);
-	auto randomNb = chanceDistrib(merseneTwister);
-	
-	for (auto i = 0u; i < probabilities.size(); i++)
-	{
-		if (randomNb <= probabilities[i])
-			return i;
-		
-		randomNb -= probabilities[i];
-	}
-	return probabilities.size() - 1;
 }
 
 struct TrainMemory
@@ -198,222 +189,40 @@ bool StateIsNone(const double* state)
 	return ::Utils::Math::Approx(state[0], NoneFlag, 0.1);
 }
 
-double PositiveMean(const std::vector<double>& values)
+struct TrainSession
 {
-	const auto sum = std::accumulate(
-		values.begin(),
-		values.end(),
-		0.0, // Init value.
-		[](const auto val1, const auto val2)
-		{
-			return val1 + std::abs(val2);
-		});
-	return sum / values.size();
-}
-
-double ComputePartEqualityEps(const std::vector<double>& values, double percentage)
-{
-	return PositiveMean(values) * percentage;
-}
-
-json WeightsToJson(const ClientSession& session, const std::vector<Output>& vars)
-{
-	std::vector<Tensor> tfOuts;
+	double episodeReward = 0;
+	int stepsWithoutFood = 0;
 	
-	TF_CHECK_OK(session.Run(vars, &tfOuts));
-
-	std::vector<std::vector<float>> varValues;
-	for (auto& tfOut : tfOuts)
-	{
-		const float* flatData = tfOut.flat<float>().data();
-		const auto totalNumParameters = tfOut.dim_size(0) * tfOut.dim_size(1);
-		
-		varValues.push_back(std::vector<float>(flatData, flatData + totalNumParameters));
-	}
-	
-	return json(varValues);
-}
-
-void JsonToWeights(
-	ClientSession& session,
-	Scope& scope,
-	const json& data,
-	std::vector<Output>& vars)
-{
-	auto varValues = data.get<std::vector<std::vector<float>>>();
-	assert(varValues.size() == vars.size());
-	
-	std::vector<Tensor> tfOuts;
-	TF_CHECK_OK(session.Run(vars, &tfOuts));
-	
-	for (auto i = 0u; i < vars.size(); i++)
-	{
-		const auto totalNumParameters = tfOuts[i].dim_size(0) * tfOuts[i].dim_size(1);
-		assert(varValues[i].size() == totalNumParameters);
-		
-		auto paramsTensor = Tensor(
-			DataTypeToEnum<float>::v(),
-			TensorShape{ tfOuts[i].dim_size(0), tfOuts[i].dim_size(1) }
-		);
-		
-		std::copy_n(varValues[i].begin(), totalNumParameters, paramsTensor.flat<float>().data());
-		auto assign = Assign(scope, vars[i], paramsTensor, { 0, 1 });
-		
-		TF_CHECK_OK(session.Run({ assign }, nullptr));
-	}
-}
-
-void TrySaveWeights(std::string fileName, const ClientSession& session, const std::vector<Output>& vars)
-{
-	std::ofstream stream(fileName);
-	
-	if (!stream.is_open())
-	{
-		std::cout << "[Warning]: " << "Failed to open " << fileName << std::endl;
-		return;
-	}
-	
-	stream << std::setw(2) << WeightsToJson(session, vars);
-	stream.close();
-}
-
-void TryLoadWeights(
-	std::string fileName,
-	ClientSession& session,
-	Scope& scope,
-	std::vector<Output>& vars)
-{
-	std::ifstream stream;
-	
-	stream.open(fileName);
-	if (!stream.is_open())
-	{
-		std::cout << "[Warning]: " << "Failed to load TF network from " << fileName << std::endl;
-		return;
-	}
-	
-	json j;
-	stream >> j;
-	
-	JsonToWeights(session, scope, j, vars);
-	stream.close();
-	
-	std::cout << "Loaded: " << std::endl << std::setw(2) << j << std::endl;
-}
+	TrainMemory trainMemory;
+};
 
 int main()
 {
-	const auto learningRate = 0.0001;
+	auto scopePtr = std::make_shared<Scope>(Scope::NewRootScope());
+	auto sessionPtr = std::make_shared<ClientSession>(*scopePtr);
 
-	auto cellInterpreter = std::make_shared<CellInterpreter::Basic3CellInterpreter>();
-	auto observer = std::make_shared<GridObserver>(cellInterpreter, 5, 5);
-//	auto observer = std::make_shared<FullMapObserver>(cellInterpreter, 25 * 25);
-	
-	const int numInputs = observer->NbOfObservations();
-	const int numOutputs = IPlayer::possibleMoves.size();
-	
-	auto scope = Scope::NewRootScope();
+	auto cellInterpreter1 = std::make_shared<CellInterpreter::Basic3CellInterpreter>();
+	auto observer1 = std::make_shared<GridObserver>(cellInterpreter1, 5, 5);
+	auto agent1 = std::make_shared<NeuralQAgent>(scopePtr, sessionPtr, observer1);
 
-	auto inputHolder = Placeholder(scope, tensorflow::DataType::DT_FLOAT);
-	
-	// Weights.
-	std::vector<int> layersShape{
+	auto agent1WeightsFile = std::string("./aux_files/qneural/network.json");
+	try
 	{
-		numInputs,
-		numInputs * 2,
-//		numInputs * 2,
-		numOutputs,
-	}};
-	
-	std::vector<Variable> weights;
-	std::vector<Assign> weightsAssign;
-	std::vector<Variable> biasses;
-	std::vector<Assign> biassesAssign;
-	
-	for (auto i = 0u; i < layersShape.size() - 1; i++)
-	{
-		auto w = Variable(scope, { layersShape[i], layersShape[i + 1] }, DataType::DT_FLOAT);
-		auto assignW = Assign(scope, w, RandomNormal(scope, { layersShape[i], layersShape[i + 1] }, DataType::DT_FLOAT));
-		
-		auto b = Variable(scope, { 1, layersShape[i + 1] }, DataType::DT_FLOAT);
-		auto assignB = Assign(scope, b, RandomNormal(scope, { 1, layersShape[i + 1] }, DataType::DT_FLOAT));
-		
-		weights.emplace_back(w);
-		weightsAssign.emplace_back(assignW);
-		
-		biasses.emplace_back(b);
-		biassesAssign.emplace_back(assignB);
+		agent1->LoadWeights(agent1WeightsFile);
 	}
-	
-	auto varIndx = 0u;
-	auto layer1 = Elu(scope, Add(scope, MatMul(scope, inputHolder, weights[varIndx]), biasses[varIndx])); varIndx++;
-//	auto layer2 = Elu(scope, Add(scope, MatMul(scope, layer1, weights[varIndx]), biasses[varIndx])); varIndx++;
-	auto layer2= Add(scope, MatMul(scope, layer1, weights[varIndx]), biasses[varIndx]); varIndx++;
-	auto& networkOutput = layer2;
-	
-	auto targetQHolder = Placeholder(scope, DataType::DT_FLOAT);
-	
-	auto loss = ReduceMean(scope,
-		Square(scope, Sub(scope, networkOutput, targetQHolder)),
-		{ 0, 1 }
-	);
-	
-	/*
-	** Backpropagation
-	*/
-	
-	std::vector<Output> gradOutputs;
-	auto variables = std::vector<Output>();
-	for (auto& w : weights)
-		variables.push_back(w);
-	for (auto& b : biasses)
-		variables.push_back(b);
-	
-	TF_CHECK_OK(AddSymbolicGradients(
-		scope,
-		{ loss },
-		variables,
-		&gradOutputs));
-
-	std::vector<Output> appliedGrads;
-	for (auto i = 0u; i < variables.size(); i++)
+	catch (...)
 	{
-		auto appliedGrad = ApplyGradientDescent(
-			scope,
-			variables[i],
-			Cast(scope, learningRate, DataType::DT_FLOAT), { gradOutputs[i] });
-		appliedGrads.emplace_back(appliedGrad);
+		std::cout << "[Warning] Failed to load network" << std::endl;
 	}
-
-	ClientSession session(scope);
-
-	// Init the weights and biasses by running the assigns nodes once.
-	std::vector<Output> assignedVars;
-	for (auto& w : weightsAssign)
-		assignedVars.push_back(w);
-	for (auto& b : biassesAssign)
-		assignedVars.push_back(b);
-	TF_CHECK_OK(session.Run(assignedVars, nullptr));
-
-	const std::string tfNetworkJsonFileName = "tfNetwork.json";
-	TryLoadWeights(tfNetworkJsonFileName, session, scope, variables);
-	
-	std::vector<Tensor> testWWW;
-	session.Run({ weights[0] }, &testWWW);
-	
-	std::cout << testWWW[0].DebugString() << std::endl;
-	
-	/*
-	** -------------------------------------------- End of network declaration -
-	*/
 
 	auto gmOptions = GetGameOptions();
 	auto qoptions = GetQOptions();
 
-	auto agents = std::vector<std::shared_ptr<GymEnv::SnakeStudent>>(
-	{
-		std::make_shared<GymEnv::SnakeStudent>()
-	});
+	auto agents = std::vector<std::shared_ptr<NeuralQAgent>>
+	{{
+		agent1
+	}};
 
 	auto players = std::vector<std::shared_ptr<IPlayer>>();
 	for (auto agent : agents)
@@ -422,30 +231,32 @@ int main()
 	auto game = Game(gmOptions, players);
 	
 	auto merseneTwister = std::mt19937(std::random_device()());
-	auto chanceDistrib = std::uniform_real_distribution<double>(0.0, 1.0);
-	auto actionDistrib = std::uniform_int_distribution<>(0, IPlayer::possibleMoves.size());
-	
-	auto observations = std::vector<double>(observer->NbOfObservations());
-	auto nextObservations = std::vector<double>(observer->NbOfObservations());
 	auto requiredOutput = std::vector<float>(IPlayer::possibleMoves.size());
 	auto startedToRender = false;
 
 	auto noise = qoptions.maxNoise;
-	auto networkOutCpy = std::vector<float>(numOutputs);
-
 	if (g_debugging)
 		std::cout << std::endl << std::endl;
 
 	for (auto episode = 0u; episode < qoptions.numEpisodes; episode++)
 	{
+		if ((episode >= qoptions.numEpisodes - qoptions.lastNGamesToRender) && !startedToRender)
+		{
+			startedToRender = true;
+			std::system("play -q -n synth 1 sin 880");
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		}
+	
 		game.InitGame();
 		const auto maxNbOfSteps = qoptions.maxNumSteps(episode);
 		const auto maxStepsWithoutFood = qoptions.maxStepsWithoutFood(episode);
 
-		auto stepsWithoutFood = 0u;
-		auto episodeReward = 0.0;
-		
-		auto episodeMemory = TrainMemory();
+		auto trainSessions = std::unordered_map<int, TrainSession>();
+		for (auto& agent : agents)
+		{
+			trainSessions[agent->GetSnakeNumber()] = TrainSession();
+		}
+
 		for (auto step = 0u; step < maxNbOfSteps; step++)
 		{
 			if (game.EveryoneIsDead())
@@ -455,191 +266,130 @@ int main()
 			{
 				const auto snakeId = agent->GetSnakeNumber();
 				const auto gmState = game.GetGameState();
+				auto& trainSession = trainSessions[snakeId];
 				
 				if (!gmState.GetSnake(snakeId).IsAlive())
 					continue;
 				
-				if (stepsWithoutFood >= maxStepsWithoutFood)
+				if (trainSession.stepsWithoutFood >= maxStepsWithoutFood)
 				{
-					std::cout << snakeId << " starved to death. ----- " << std::endl;
 					game.ForcefullyKillPlayer(snakeId);
 					continue;
 				}
 				
-				observer->Observe(observations, gmState, snakeId);
-				auto observationTensor = Tensor(
-					DataTypeToEnum<float>::v(),
-					TensorShape{ 1, (int)observer->NbOfObservations() }
-				);
-				std::copy_n(
-					observations.begin(),
-					observer->NbOfObservations(),
-					observationTensor.flat<float>().data());
-				
-				std::vector<Tensor> tfOutputs;
-				TF_CHECK_OK(session.Run(
-					{{ inputHolder, observationTensor }},
-					{ networkOutput },
-					&tfOutputs));
-				const float* actionsQ = tfOutputs[0].flat<float>().data();
-
-				int actionIndx = 0;
-				auto networkOutDbl = std::vector<double>(actionsQ, actionsQ + numOutputs);
-				
-				const auto positiveMean = PositiveMean(networkOutDbl);
-				actionIndx = AI::QLearning::Utils::PickActionAdditiveNoise(
-					networkOutDbl,
-					positiveMean * noise,
+				int actionIndx = agent->GetNextActionIndex(
+					gmState,
+					noise,
 					merseneTwister,
-					positiveMean * qoptions.actionQualityEps);
-				
-				if (g_debugging)
-				{
-					std::cout << "\033[33m[NetworkOutput]:" << tfOutputs[0].DebugString() << "\033[0m" << std::endl;
-				}
+					qoptions.actionQualityEps);
 
 				if (episode >= qoptions.numEpisodes - qoptions.lastNGamesToRender)
 				{
-					if (!startedToRender)
-					{
-						startedToRender = true;
-						std::system("play -q -n synth 1 sin 880");
-						std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-						qoptions.minNoise = 0;
-					}
 					gmOptions.gameRenderer->Render(game.GetGameState());
 					if (qoptions.milsToSleepBetweenFrames != 0)
-					{
 						std::this_thread::sleep_for(std::chrono::milliseconds(qoptions.milsToSleepBetweenFrames));
-					}
 				}
 				
 				const auto action = IPlayer::possibleMoves[actionIndx];
 				const auto rawReward = game.MoveSnake(snakeId, action);
 
-				auto reward = 0.0;
-				if (rawReward < 0)
-				{
-					reward += qoptions.dieReward(episode);
-				}
-				else if (rawReward > 0)
-				{
-					reward += qoptions.foodReward(episode);
-					stepsWithoutFood = 0;
-				}
-				else
-				{
-					reward += qoptions.stepReward(episode);
-					stepsWithoutFood++;
-				}
+				auto reward = qoptions.ComputeReward(episode, rawReward);
+				if (rawReward > 0)
+					trainSession.stepsWithoutFood = 0;
+				else if (rawReward == 0)
+					trainSession.stepsWithoutFood++;
 
-				if (g_debugging)
-					std::cout << "\033[34mReward: " << reward << "\033[0m" << std::endl;
-
-				episodeReward += reward;
+				trainSession.episodeReward += reward;
 				const auto isDone = (rawReward < 0);
 				
 				if (isDone)
-				{
-					nextObservations[0] = NoneFlag;
-				}
+					agent->nextObservationContainer[0] = NoneFlag;
 				else
 				{
 					const auto gmStatePrim = game.GetGameState();
-					observer->Observe(nextObservations, gmStatePrim, snakeId);
+					agent->observer->Observe(agent->nextObservationContainer, gmStatePrim, snakeId);
 				}
-				episodeMemory.RegisterMemory(observations, actionIndx, reward, nextObservations);
+				trainSession.trainMemory.RegisterMemory(
+					agent->observationContainer,
+					actionIndx,
+					reward,
+					agent->nextObservationContainer);
 				game.RestockFood();
 			}
 		}
 		
-		const auto discountedRewards = episodeMemory.GetDiscountedRewards(qoptions.qDiscountFactor);
-		for (int batch_i = episodeMemory.nbOfMemories - 1; batch_i >= 0; batch_i--)
+		for (auto& agent : agents)
 		{
-			const auto batchSize = 1;
-
-			std::vector<std::vector<double>> observationsBatch = { episodeMemory.states[batch_i] };
-			auto statesBatch = MakeTensor(observationsBatch);
-			std::vector<Tensor> q_s_a;
-			TF_CHECK_OK(session.Run(
-				{{ inputHolder, statesBatch }},
-				{ networkOutput },
-				&q_s_a
-			));
-
-			std::vector<std::vector<double>> nextObservationsBatch = { episodeMemory.nextStates[batch_i] };
-			auto nextStatesBatch = MakeTensor(nextObservationsBatch);
-			std::vector<Tensor> q_s_a_d;
-			TF_CHECK_OK(session.Run(
-				{{ inputHolder, nextStatesBatch }},
-				{ networkOutput },
-				&q_s_a_d
-			));
-
-			auto trainOutputBatch = Tensor(
-				DataTypeToEnum<float>::v(),
-				TensorShape{ (int)batchSize, (int)numOutputs });
-
-			std::copy_n(
-				q_s_a[0].matrix<float>().data(),
-				batchSize * numOutputs,
-				trainOutputBatch.matrix<float>().data()
-			);
-
-			const auto actionIndx = episodeMemory.actions[batch_i];
-			float finalActionReward = discountedRewards[batch_i];
-//			float finalActionReward = episodeMemory.rewards[batch_i];
-
-			if (!StateIsNone(episodeMemory.nextStates[batch_i].data()))
+			auto& trainSession = trainSessions[agent->GetSnakeNumber()];
+			auto& trainMemory = trainSession.trainMemory;
+		
+			const auto discountedRewards = trainMemory.GetDiscountedRewards(qoptions.qDiscountFactor);
+			for (int batch_i = trainMemory.nbOfMemories - 1; batch_i >= 0; batch_i--)
 			{
-				const float* thisBatch_q_s_a_d = q_s_a_d[0].matrix<float>().data();
-				const auto bestNextQ = *std::max_element(thisBatch_q_s_a_d, thisBatch_q_s_a_d + numOutputs);
-				finalActionReward += qoptions.qDiscountFactor * bestNextQ;
-			}
-			trainOutputBatch.matrix<float>()(actionIndx) = finalActionReward;
+				const auto batchSize = 1;
 
-			// Train.
-			{
-				auto stuffToBeComputed = std::vector<Output>();
-				for (auto& appliedGrad : appliedGrads)
-					stuffToBeComputed.push_back(appliedGrad);
-				stuffToBeComputed.push_back(networkOutput);
-				stuffToBeComputed.push_back(loss);
+				std::vector<std::vector<double>> observationsBatch = { trainMemory.states[batch_i] };
+				std::vector<std::vector<double>> nextObservationsBatch = { trainMemory.nextStates[batch_i] };
+				
+				auto inputBatch = MakeTensor(observationsBatch);
+				auto q_s_a = agent->PredictBatch(inputBatch);
+				auto q_s_a_d = agent->PredictBatch(MakeTensor(nextObservationsBatch));
 
-				std::vector<Tensor> tfOutput;
-				TF_CHECK_OK(session.Run(
-					{
-						{ inputHolder, statesBatch },
-						{ targetQHolder, trainOutputBatch }
-					},
-					stuffToBeComputed,
-					&tfOutput
-				));
+				auto trainOutputBatch = Tensor(
+					DataTypeToEnum<float>::v(),
+					TensorShape{ (int)batchSize, (int)agent->NumOutputs() });
 
-				if (g_debugging)
+				std::copy_n(
+					q_s_a.matrix<float>().data(),
+					batchSize * agent->NumOutputs(),
+					trainOutputBatch.matrix<float>().data()
+				);
+
+				const auto actionIndx = trainMemory.actions[batch_i];
+				float finalActionReward = discountedRewards[batch_i];
+				
+				if (!StateIsNone(trainMemory.nextStates[batch_i].data()))
 				{
-					std::cout
-						<< "------- [LOSS]:"
-						<< std::fixed << std::setprecision(2)
-						<< tfOutput.back().flat<float>()(0)
-						<< std::endl;
+					const float* q_s_a_d_raw = q_s_a_d.matrix<float>().data();
+					const auto bestNextQ = *std::max_element(
+						q_s_a_d_raw,
+						q_s_a_d_raw + agent->NumOutputs());
+
+					finalActionReward += qoptions.qDiscountFactor * bestNextQ;
 				}
+				trainOutputBatch.matrix<float>()(actionIndx) = finalActionReward;
+				
+				agent->Train(inputBatch, trainOutputBatch);
 			}
 		}
-		
-		printf(
-			"----------------- End of episode: %d with a reward of\033[32m %.2f\033[0m. Noise: %.2f. Die reward: %.2f\n",
-			episode,
-			episodeReward,
+
+		printf("End of episode: %5d. Die reward: %3.2f\n", episode, qoptions.dieReward(episode));
+		for (const auto& agent : agents)
+		{
+			const auto& trainSession = trainSessions[agent->GetSnakeNumber()];
+			printf("- %d) Reward =\033[32m %5.2f\033[0m. Noise = %2.2f %s\n",
+				agent->GetSnakeNumber(),
+				trainSession.episodeReward,
+				noise,
+				(trainSession.stepsWithoutFood >= maxStepsWithoutFood) ? "\033[31m - Starved - \033[0m" : "");
+		}
+		noise = ::Utils::Math::Lerp(
 			noise,
-			qoptions.dieReward(episode));
-		noise = ::Utils::Math::Lerp(noise, qoptions.minNoise, qoptions.noiseDecayFactor);
-		
-//		char c;
-//		std::cin >> c;
+			qoptions.minNoise,
+			qoptions.noiseDecayFactor);
 	}
 	
-	TrySaveWeights(tfNetworkJsonFileName, session, variables);
+	try
+	{
+		agent1->SaveWeights(agent1WeightsFile);
+	}
+	catch (...)
+	{
+		// Ignore.
+	}
+	
+	auto j = json(agent1.get());
+	auto test = j.get<std::shared_ptr<NeuralQAgent>>();
 	
 	return 0;
 }
