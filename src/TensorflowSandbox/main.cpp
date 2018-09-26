89673180091d605b2d46110bac5ef30746078e78
Emil Terman <terman.emil@gmail.com>
2018-09-26 10:53:13 +0300

    Separated TrainMemory in a different file

--- src/TensorflowSandbox/main.cpp
+++ src/TensorflowSandbox/main.cpp
@@ -1,4 +1,5 @@
 #include "NeuralQAgent.hpp"
+#include "TrainMemory.h"
 
 #include "GameLogic/CellInterpreter/Basic3CellInterpreter.hpp"
 #include "GymEnv/StateObserver/GridObserver.hpp"
@@ -125,46 +126,6 @@ QOptions GetQOptions()
 
 	return qoptions;
 }
-
-struct TrainMemory
-{
-	std::vector<std::vector<double>> states;
-	std::vector<int> actions;
-	std::vector<double> rewards;
-	std::vector<std::vector<double>> nextStates;
-	
-	int nbOfMemories = 0;
-	
-	void RegisterMemory(
-		const std::vector<double>& state,
-		int action,
-		double reward,
-		const std::vector<double>& nextState)
-	{
-		states.push_back(state);
-		actions.push_back(action);
-		rewards.push_back(reward);
-		nextStates.push_back(nextState);
-		
-		nbOfMemories++;
-	}
-	
-	std::vector<float> GetDiscountedRewards(double qDiscountFactor)
-	{
-		std::vector<float> discountedRewards;
-		discountedRewards.reserve(rewards.size());
-		
-		float lastReward = 0;
-		for (auto it = rewards.rbegin(); it != rewards.rend(); it++)
-		{
-			lastReward = lastReward * qDiscountFactor + (*it);
-			discountedRewards.push_back(lastReward);
-		}
-		std::reverse(discountedRewards.begin(), discountedRewards.end());
-		
-		return discountedRewards;
-	}
-};
 
 Tensor MakeTensor(std::vector<std::vector<double>>& data)
 {
